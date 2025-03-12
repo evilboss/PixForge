626fada0917b7c2a4bec1425f23419040d83a961
evilboss <jr.evilboss@gmail.com>
2025-03-12 19:33:46 +0800

    change gateway startup mesage

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
@@ -8,7 +9,7 @@ async function bootstrap() {
 
   const port = configService.get<number>('API_GATEWAY_PORT') || 4005;
   await app.listen(port);
-  console.log(`API Gateway is listening on port ${port}`);
+  console.log(`API Gateway runs on port ${port}`);
 }
 
 bootstrap();
